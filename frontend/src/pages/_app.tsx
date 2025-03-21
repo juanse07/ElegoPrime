547999135eb8339ca2a626bb505aca08d5cff5ac
juan <juan2007@gmail.com>
2025-03-21 10:41:31 -0600

    metadata image optimized2

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -36,16 +36,16 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="description" content="Elego Prime is a experienced handyman service that provides a variety of services to the Denver area home owners and businesses." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="logoelegotab.svg" sizes="32x32" />
-        <meta property="og:image" content="https://elegoprime.com/crew1.webp" />
+        <meta property="og:image" content="https://elegoprime.com/crew1MetaDataOptimized.webp" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Elego Prime" />
         <meta name="twitter:description" content="Elego Prime is a experienced handyman service that provides a variety of services to the Denver area home owners and businesses." />
-        <meta name="twitter:image" content="https://elegoprime.com/crew1.webp" />
+        <meta name="twitter:image" content="https://elegoprime.com/crew1MetaDataOptimized.webp" />
       </Head>
       <NextNProgress
           color="#29D"
           startPosition={0.3}
-          stopDelayMs={200}
+          stopDelayMs={200}ß
           height={3}   
           showOnShallow={true}    
          />
