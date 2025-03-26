5095ef06eb627d937ed5118e687694a2d8016e37
Paula <140281997+ovenuso@users.noreply.github.com>
2025-03-26 12:43:50 -0500

    video mobile version, reserve icon button on cards added

--- frontend/src/components/HandyServiceSection.tsx
+++ frontend/src/components/HandyServiceSection.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import styles from '../styles/handyService.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
 
 interface Subservice {
   label: string;
@@ -23,40 +25,30 @@ const HandyServiceSection: React.FC = () => {
       label: 'Security',
       subservices: [
         { label: 'Camera install', description: 'Security camera installation and maintenance.', image:'/camerahome.webp'},
-        // { label: 'Doorbell install', description: 'Install and maintain doorbell systems.', image:'/security-system.webp' },
       ]
     },
     {
       label: 'SoundBars',
       subservices: [
         { label: 'Soundbars install', description: 'Immersive sound in your home with professional soundbar installation', image: '/soundbarhome.webp' },
-        // { label: 'Videobeam projectors', description: 'High-quality projectors.', image: '/herramientasmovil2.webp'}
       ]
     },
     {
       label: 'TV',
       subservices: [
         { label: 'Tv install', description: 'Professional TV mounting and installation for a sleek, secure setup in any room.', image: '/tvinstallhome.webp' },
-        // { label: 'Overhead TV', description: 'Expert installation of overhead TVs for an elevated viewing experience and space-saving design.', image: '/camerainstall.webp' },
-        // { label: 'Electric base TV', description: 'Installation and maintenance of electric base TVs, ensuring smooth operation and durability.', image: '/herramientasmovil2.webp' },
-        // { label: 'Hide cables', description: 'Neat and efficient cable management to hide and organize wires for a clean and tidy space.', image: '/muchasherramientas.webp' }
       ]
     },
     {
       label: 'Fans and Lighting',
       subservices: [
         { label: 'Fans install', description: 'Professional installation of ceiling or stand fans to ensure proper air circulation and comfort.', image: '/fanshome.webp' },
-        // { label: 'Lamps and lights install', description: 'Expert installation of lamps and light fixtures, enhancing the ambiance and lighting in your space.', image: '/muchasherramientas.webp' }
       ]
     }
   ];
 
   const toggleExpand = (serviceLabel: string) => {
-    if (expandedCard === serviceLabel) {
-      setExpandedCard(null);
-    } else {
-      setExpandedCard(serviceLabel);
-    }
+    setExpandedCard(expandedCard === serviceLabel ? null : serviceLabel);
   };
 
   return (
@@ -78,7 +70,6 @@ const HandyServiceSection: React.FC = () => {
                 href={`/category?service=${encodeURIComponent(service.label)}&subservice=${encodeURIComponent(service.subservices[0].label)}`}
                 className="text-decoration-none"
                 onClick={(e) => {
-                  // For mobile: prevent navigation on first click to allow expanding
                   if (window.innerWidth <= 768) {
                     e.preventDefault();
                     toggleExpand(service.label);
@@ -96,8 +87,16 @@ const HandyServiceSection: React.FC = () => {
                     />
                   </div>
                   <div className={styles.content}>
-                    <div className={styles.label}>{service.subservices[0].label}</div>
-                    <h3 className={styles.title}>{service.label}</h3>
+                    <div className={styles.labelcontainer}>
+                      <div className={styles.label}>{service.subservices[0].label}</div>
+                        <Link href="/reserve" className={styles.calendarIcon}>
+                        <FontAwesomeIcon icon={faCalendarDays} />
+                        </Link>
+                    </div>
+
+                  <div className={styles.titleContainer}>
+                  <h3 className={styles.title}>{service.label}</h3>
+                </div>
                     <p className={styles.description}>
                       {service.subservices[0].description}
                     </p>
